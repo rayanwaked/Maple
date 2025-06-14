a0c95349c6c2b18f12b16cbc33d9f5515d9548ca
Rayan Waked <rayan@waked.dev>
2025-06-14 13:01:57 -0700

    Initial Commit

---
+++ Maple/MapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapleApp.swift
+//  Maple
+//
+//  Created by Rayan Waked on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
